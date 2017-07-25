073fff24aa626c7e15b6574810667f5b0ddad7da
DaveParillo <DaveParillo@users.noreply.github.com>
2017-07-25 14:52:40 -0700

    part of #5 - added standard apache header

--- src/main/java/com/canoo/webtest/steps/store/StoreURLDecoded.java
+++ src/main/java/com/canoo/webtest/steps/store/StoreURLDecoded.java
@@ -1,5 +1,16 @@
 // Copyright 2017 Dave Parillo
-// Released under the Apache license v2.0.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.canoo.webtest.steps.store;
 
 import java.io.UnsupportedEncodingException;
@@ -15,6 +26,7 @@ import org.apache.tools.ant.BuildException;
  *   category="Core"
  *   name="storeURLDecoded"
  *   description="Convert text to it's URLDecoded equivalent and store the result into a property."
+ * @since July 2017
  */
 public class StoreURLDecoded extends BaseStoreStep {
     private String fText;
@@ -36,6 +48,7 @@ public class StoreURLDecoded extends BaseStoreStep {
         super.setProperty(property);
     }
 
+    @Override
     public void doExecute() {
         try {
             storeProperty(URLDecoder.decode(fText, enc));
@@ -44,6 +57,7 @@ public class StoreURLDecoded extends BaseStoreStep {
         }
     }
 
+    @Override
     protected void verifyParameters() {
         super.verifyParameters();
         emptyParamCheck(fText, "text");
--- src/main/java/com/canoo/webtest/steps/store/StoreURLEncoded.java
+++ src/main/java/com/canoo/webtest/steps/store/StoreURLEncoded.java
@@ -1,5 +1,16 @@
 // Copyright 2017 Dave Parillo
-// Released under the Apache license v2.0.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.canoo.webtest.steps.store;
 
 import java.io.UnsupportedEncodingException;
@@ -15,6 +26,7 @@ import org.apache.tools.ant.BuildException;
  *   category="Core"
  *   name="storeURLEncoded"
  *   description="Convert text to it's URLEncoded equivalent and store the result into a property."
+ * @since July 2017
  */
 public class StoreURLEncoded extends BaseStoreStep {
     private String fText;
@@ -36,6 +48,7 @@ public class StoreURLEncoded extends BaseStoreStep {
         super.setProperty(property);
     }
 
+    @Override
     public void doExecute() {
         try {
             storeProperty(URLEncoder.encode(fText, enc));
@@ -44,6 +57,7 @@ public class StoreURLEncoded extends BaseStoreStep {
         }
     }
 
+    @Override
     protected void verifyParameters() {
         super.verifyParameters();
         emptyParamCheck(fText, "text");
